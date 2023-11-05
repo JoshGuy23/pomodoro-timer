c52f6a40e47f3e6246d4ac435f416bc8b9a2ee7a
Joshua Hecker <jhecker2001@gmail.com>
2023-11-05 09:45:31 -0600

    Edited structure of program and added countdown mechanism.

--- main.py
+++ main.py
@@ -14,55 +14,39 @@ CHECKMARK = "✓"
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    canvas.itemconfig(timer_text, text=f"{count}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+image = PhotoImage(file="tomato.png")
 
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
-def create_button(txt):
-    button = Button(text=txt, highlightthickness=0)
-    return button
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW)
+timer_label.config(font=(FONT_NAME, 50))
+timer_label.grid(column=1, row=0)
 
+check_label = Label(text=CHECKMARK, fg=GREEN, bg=YELLOW)
+check_label.grid(column=1, row=3)
 
-def create_label(txt):
-    label = Label(text=txt, fg=GREEN, bg=YELLOW)
-    return label
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
 
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
 
-def setup_window():
-    window = Tk()
-    window.title("Pomodoro")
-    window.config(padx=100, pady=50, bg=YELLOW)
-    return window
+count_down(5)
 
-
-def create_canvas(img):
-    canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-    canvas.create_image(100, 112, image=img)
-    canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-    return canvas
-
-
-def main():
-    window = setup_window()
-    image = PhotoImage(file="tomato.png")
-
-    canvas = create_canvas(image)
-    canvas.grid(column=1, row=1)
-
-    timer_label = create_label("Timer")
-    timer_label.config(font=(FONT_NAME, 50))
-    timer_label.grid(column=1, row=0)
-
-    check_label = create_label(CHECKMARK)
-    check_label.grid(column=1, row=3)
-
-    start_button = create_button("Start")
-    start_button.grid(column=0, row=2)
-
-    reset_button = create_button("Reset")
-    reset_button.grid(column=2, row=2)
-    window.mainloop()
-
-
-main()
+window.mainloop()
