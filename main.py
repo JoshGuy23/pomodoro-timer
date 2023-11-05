1bd74407171f188f0773471a31c0a73e05e3bab4
Joshua Hecker <jhecker2001@gmail.com>
2023-11-05 09:53:07 -0600

    Added timer mechanism and functionality to display time remaining in minutes and seconds.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,13 +13,20 @@ CHECKMARK = "✓"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    count_down(WORK_MIN * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=f"{count}")
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -30,7 +38,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 image = PhotoImage(file="tomato.png")
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-canvas.create_image(100, 112, image=img)
+canvas.create_image(100, 112, image=image)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
@@ -41,12 +49,10 @@ timer_label.grid(column=1, row=0)
 check_label = Label(text=CHECKMARK, fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
-
-count_down(5)
 
 window.mainloop()
