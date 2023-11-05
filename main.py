c95f7a50e4bf1fdfcb2e48a0f79cbc285a57e549
Joshua Hecker <jhecker2001@gmail.com>
2023-11-05 10:32:14 -0600

    Added functionality to reset the application.

--- main.py
+++ main.py
@@ -11,8 +11,19 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECKMARK = "✓"
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer", fg=GREEN)
+    check_label.config(text="")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
@@ -20,9 +31,9 @@ reps = 0
 def start_timer():
     global reps
     reps += 1
-    work_sec = 10
-    short_break_sec = 5
-    long_break_sec = 15
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
         timer_time = long_break_sec
@@ -44,14 +55,14 @@ def start_timer():
 
 
 def count_down(count):
-    global reps
+    global reps, timer
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
     canvas.itemconfig(timer_text, text=f"{count_min:02d}:{count_sec:02d}")
 
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
 
@@ -60,7 +71,6 @@ def count_down(count):
         for _ in range(sessions):
             marks += CHECKMARK
         check_label.config(text=marks)
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -84,7 +94,7 @@ check_label.grid(column=1, row=3)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 window.mainloop()
